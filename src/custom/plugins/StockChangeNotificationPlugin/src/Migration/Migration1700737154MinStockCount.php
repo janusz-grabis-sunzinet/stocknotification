2e5a3343f5ebef3714060635d9534a953a1df57f
Janusz Grabis <janusz@frameworks.pl>
2023-11-27 09:16:49 +0100

    Checking if column does not exist before adding it.

--- src/custom/plugins/StockChangeNotificationPlugin/src/Migration/Migration1700737154MinStockCount.php
+++ src/custom/plugins/StockChangeNotificationPlugin/src/Migration/Migration1700737154MinStockCount.php
@@ -14,12 +14,19 @@ class Migration1700737154MinStockCount extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $query = <<<SQL
+
+        $query = "SELECT COUNT(*) as column_count FROM information_schema.columns WHERE table_name = 'ware_email' AND column_name = 'min_stock_count'";
+        $result = $connection->executeQuery($query);
+        $row = $result->fetchAssociative();
+        $columnCount = $row['column_count'];
+
+        if ($columnCount == 0) {
+            $query = <<<SQL
 ALTER TABLE `ware_email` 
 ADD COLUMN `min_stock_count` INT NOT NULL DEFAULT 1;
-
 SQL;
-        $connection->executeStatement($query);
+            $connection->executeStatement($query);
+        }
     }
 
     public function updateDestructive(Connection $connection): void
